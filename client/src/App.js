dfe6901df90a0a61f7aff5f66403e152fa2d227f
Sudharsan G S <g.s.sudharsan99@gmail.com>
2020-03-24 19:55:46 +0530

    Added backend auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import './App.css';
-//import Mapss from './components/maps';
-//import Signup from './components/signup';
 import Home from './components/home/home';
-import Login from './components/login';
+
 
 function App() {
   return(
--- client/src/components/home/home.jsx
+++ client/src/components/home/home.jsx
@@ -6,8 +6,8 @@ export default function Home(){
     return(
         <div className="wrapper">
             <h1>Fixian</h1>
-            <div>
-                
+            <div className="element">
+                <img src=""/>
             </div>
         </div>
     )

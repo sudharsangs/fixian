79359fae286ea9f024a7499dbe4305973530d169
Sudharsan G S <g.s.sudharsan99@gmail.com>
2020-05-18 19:03:01 +0530

    Added Server

--- client/src/components/home/home.jsx
+++ client/src/components/home/home.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+//import {Link,  withRouter} from 'react-router-dom';
+import customer from '../../assets/customer.png';
 import './home.css';
 
 
-export default function Home(){
+ function Home(){
     return(
         <div className="wrapper">
             <h1>Fixian</h1>
-            <div className="element">
-                <img src=""/>
-            </div>
+                
+                     <div className="element">
+                       <img src={customer} className="customer"/>
+                     </div>
+              
+            
+            
         </div>
     )
 }
+
+export default Home;
